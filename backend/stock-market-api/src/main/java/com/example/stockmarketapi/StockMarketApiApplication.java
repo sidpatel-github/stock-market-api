a9156526a4c0705162221d60854104c5adc59fc8
siddharth <siddharth.github@gmail.com>
2024-01-21 16:22:27 -0500

    Stock market API initial commit

---
+++ backend/stock-market-api/src/main/java/com/example/stockmarketapi/StockMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketApiApplication.class, args);
+	}
+
+}
