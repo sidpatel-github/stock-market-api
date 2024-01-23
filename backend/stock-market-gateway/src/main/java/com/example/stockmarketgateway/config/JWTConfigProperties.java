a5c1e6c859e6c94c79344169b38fca391ddcfd2d
siddharth <siddharth.github@gmail.com>
2024-01-22 20:02:20 -0500

    jwt token configuration property class added

---
+++ backend/stock-market-gateway/src/main/java/com/example/stockmarketgateway/config/JWTConfigProperties.java
@@ -0,0 +1,12 @@
+package com.example.stockmarketgateway.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "app.jwt")
+public class JWTConfigProperties {
+    private String secret;
+}
