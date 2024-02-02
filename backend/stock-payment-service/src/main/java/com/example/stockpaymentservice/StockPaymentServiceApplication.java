6cbfdb8759e4d5069be58d70c33191ee23f43eaa
siddharth <siddharth.github@gmail.com>
2024-02-01 20:17:58 -0500

    initial commit for stock payment service

---
+++ backend/stock-payment-service/src/main/java/com/example/stockpaymentservice/StockPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ backend/stock-payment-service/src/test/java/com/example/stockpaymentservice/StockPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
