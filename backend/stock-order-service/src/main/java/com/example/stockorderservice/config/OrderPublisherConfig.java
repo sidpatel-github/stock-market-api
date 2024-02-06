a8128c9a8a23ed4a160f0dad208712371735dff8
siddharth <siddharth.github@gmail.com>
2024-02-05 19:54:52 -0500

    kafka consumer function added

--- backend/stock-order-service/src/main/java/com/example/stockorderservice/config/OrderPublisherConfig.java
+++ backend/stock-order-service/src/main/java/com/example/stockorderservice/config/OrderPublisherConfig.java
@@ -11,12 +11,13 @@ import java.util.function.Supplier;
 @Configuration
 public class OrderPublisherConfig {
     @Bean
-    public Sinks.Many<OrderEvent> orderSinks(){
+    public Sinks.Many<OrderEvent> orderSinks() {
         return Sinks.many().multicast().onBackpressureBuffer();
     }
 
     @Bean
-    public Supplier<Flux<OrderEvent>> orderSupplier(Sinks.Many<OrderEvent> sinks){
+    public Supplier<Flux<OrderEvent>> orderSupplier(Sinks.Many<OrderEvent> sinks) {
         return sinks::asFlux;
     }
+
 }
