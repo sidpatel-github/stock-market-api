af63577c32fd011a4fe36fc21ad63aeaf012483c
siddharth <siddharth.github@gmail.com>
2024-02-05 19:52:50 -0500

    indentation corrected in yml file

--- backend/stock-order-service/src/main/java/com/example/stockorderservice/repository/OrderRepository.java
+++ backend/stock-order-service/src/main/java/com/example/stockorderservice/repository/OrderRepository.java
@@ -2,9 +2,11 @@ package com.example.stockorderservice.repository;
 
 import com.example.stockorderservice.entity.PurchaseOrder;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends MongoRepository<PurchaseOrder, String> {
     List<PurchaseOrder> findByUserId(String userId);
 }
